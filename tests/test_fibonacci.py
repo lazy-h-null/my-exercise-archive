3b48c164b857ae2803c62bc12f1c93d2766a573c
y44k0v <17897299+y44k0v@users.noreply.github.com>
2026-03-12 19:45:44 -0400

    Clone of pythoneastsep15

---
+++ tests/test_fibonacci.py
@@ -0,0 +1,28 @@
+import unittest
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fibonacci import calculate_fibonacci
+
+class TestFibonacci(unittest.TestCase):
+
+    def test_fibonacci_for_zero(self):
+        self.assertEqual(calculate_fibonacci(0), 0)
+
+    def test_fibonacci_for_one(self):
+        self.assertEqual(calculate_fibonacci(1), 1)
+    
+    def test_fibonacci_for_five(self):
+        self.assertEqual(calculate_fibonacci(5), 5)
+
+    def test_fibonacci_for_ten(self):
+        self.assertEqual(calculate_fibonacci(10), 55)
+
+    def test_fibonacci_with_negative_number(self):
+        with self.assertRaises(ValueError):
+            calculate_fibonacci(-1)
+
+if __name__ == '__main__':
+    unittest.main()
